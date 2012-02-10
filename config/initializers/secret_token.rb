1f5a3a8f1826e3ddd3bf3b485883d37c24613b2d
U-JRaeford-Win7PC\JRaeford <JRaeford@JRaeford-Win7PC.(none)>
2012-02-09 21:24:56 -0500

    Initial Commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0f69391cc1f0cab098b409be8581398459c7135da5f891504af173e6319498a3549b146c26fefb33b6256e44ca70092a7e40076febfc0c7ed4bb936a543d93bf'
